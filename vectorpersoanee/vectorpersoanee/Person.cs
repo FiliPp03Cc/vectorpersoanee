60ddba95544683e5ff98a45f64af4f9220b5de81
Filip-Gheorghe Caracoancea <filip.caracoancea@emanuel.ro>
2024-01-22 10:05:51 +0100

    examen

---
+++ vectorpersoanee/vectorpersoanee/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vectorpersoanee
+{
+    internal class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public int YearOfBirth { get; set; }
+        public int Age
+        {
+            get
+            {
+
+                return DateTime.Now.Year - YearOfBirth;
+            }
+        }
+
+    }
+}
